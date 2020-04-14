50f12970e89ac0b5975776f258f91b6977b7f506
Danil Lebets <stebel99@gmail.com>
2020-04-15 02:04:37 +0300

    Add UserFunction, add variable to DeleteTask

---
+++ Practice4/AdvancedScripts/UserFunction.sql
@@ -0,0 +1,23 @@
+﻿--Функция, которая проверяет, можно ли создать поставку с определенным количеством цветов определенного типа из определённой плантации.
+--Т.е. хватает ли на плантации количества цветов с учетом того, что до того, как поставка закрыта, количество цветов на плантации остается неизменным.
+--После закрытия поставки количество цветов на плантации уменьшается. 
+--Входящие параметры: Id вида цветов, Id плантации, количество. 
+--Возвращаемое значение в формате true/false.
+USE [Practice4db]
+
+GO
+CREATE FUNCTION CanCreateSupply (@FlowerId INT, @PlantationId INT, @Amount INT)
+	RETURNS  BIT
+BEGIN
+	DECLARE @PlantationAmount INT, @Result INT
+	SET @PlantationAmount = (SELECT PF.[Amount] FROM [PlantationFlower] PF WHERE PF.FlowerId = @FlowerId AND PF.PlantationId = @PlantationId)
+	SET @Result = @PlantationAmount - @Amount
+	IF @Result >= 0
+		RETURN 1;
+	RETURN 0;
+END;
+
+----TEST
+--GO
+--SELECT P.[Name], PF.[Amount], [dbo].CanCreateSupply(3,2,301) FROM [Plantation] P
+--INNER JOIN [PlantationFlower] PF ON P.[Id] = PF.[PlantationId]
--- Practice4/Scripts/Delete/DeleteTask.sql
+++ Practice4/Scripts/Delete/DeleteTask.sql
@@ -5,11 +5,12 @@ DELETE FROM [PlantationFlower] WHERE [PlantationId] = 1 AND [FlowerId] = 1
 
 --2.Удалить данные о виде цветов из всей БД.
 USE [Practice4db]
-
-DELETE FROM [PlantationFlower] WHERE [FlowerId] = 1
-DELETE FROM [SupplyFlower] WHERE [FlowerId] = 1
-DELETE FROM [WarehouseFlower] WHERE [FlowerId] =1
-DELETE FROM [Flower] WHERE [Id] = 1
+DECLARE @DeleteId int 
+SET @DeleteId = 1
+DELETE FROM [PlantationFlower] WHERE [FlowerId] = @DeleteId
+DELETE FROM [SupplyFlower] WHERE [FlowerId] = @DeleteId
+DELETE FROM [WarehouseFlower] WHERE [FlowerId] = @DeleteId
+DELETE FROM [Flower] WHERE [Id] = @DeleteId
 
 
 
