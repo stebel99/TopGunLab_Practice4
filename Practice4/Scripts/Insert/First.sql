9a4550f864aac14414b517700c0e4b8089ec531d
Danil Lebets <stebel99@gmail.com>
2020-04-14 16:56:33 +0300

    Change file structure in Scripts\Insert add tasks to one file

--- Practice4/Scripts/Insert/First.sql
+++
@@ -1,2 +0,0 @@
-﻿--Добавим один вид цветов
-INSERT INTO [Practice4db].[dbo].[Flower]([Name]) VALUES ('Ромашка');
--- Practice4/Scripts/Insert/InsertTask.sql
+++ Practice4/Scripts/Insert/InsertTask.sql
@@ -1,4 +1,7 @@
-﻿--Добавим несколько плантаций
+﻿--Добавим один вид цветов
+INSERT INTO [Practice4db].[dbo].[Flower]([Name]) VALUES ('Ромашка');
+
+--Добавим несколько плантаций
 INSERT INTO [Practice4db].[dbo].[Plantation]  ([Name],[Adress]) 
     VALUES 
     ('Квітковий рай','Ось там'),
