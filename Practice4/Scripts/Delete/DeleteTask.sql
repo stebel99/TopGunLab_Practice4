1a6386276999b2e2b8be0d1dce44d5287f070f67
Danil Lebets <stebel99@gmail.com>
2020-04-14 19:03:33 +0300

    Add "Delete" scripts

---
+++ Practice4/Scripts/Delete/DeleteTask.sql
@@ -0,0 +1,16 @@
+﻿--1.Удалить данные о виде цветов на опредленной плантации; 
+USE [Practice4db]
+
+DELETE FROM [PlantationFlower] WHERE [PlantationId] = 1 AND [FlowerId] = 1
+
+--2.Удалить данные о виде цветов из всей БД.
+USE [Practice4db]
+
+DELETE FROM [PlantationFlower] WHERE [FlowerId] = 1
+DELETE FROM [SupplyFlower] WHERE [FlowerId] = 1
+DELETE FROM [WarehouseFlower] WHERE [FlowerId] =1
+DELETE FROM [Flower] WHERE [Id] = 1
+
+
+
+
